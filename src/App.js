ed5941d89991a07cafd892e07266592a32168dac
Paras Arya <parasarya289@gmail.com>
2023-10-02 23:06:51 +0530

    feat: initial setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { Layout } from "./Layout/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+        <h1>This is habit tracker</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
