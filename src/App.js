74575876976b6a5ac362bad06c0adc2b61724719
Paras Arya <parasarya289@gmail.com>
2023-10-03 00:20:47 +0530

    feat: router setup and layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import { Layout } from "./Layout/Layout";
+import { Dashboard } from "./Pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
-        <h1>This is habit tracker</h1>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/exercises" element={<h1>Exercises</h1>}/>
+          <Route path="/foods" element={<h1>Foods</h1>}/>
+          <Route path="/goals" element={<h1>Goals</h1>}/>
+          <Route path="*" element={<h1>Page does not exist</h1>}/>
+        </Routes>
       </Layout>
     </div>
   );
