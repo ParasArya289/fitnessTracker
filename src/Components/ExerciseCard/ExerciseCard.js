7c088adfa7a98a697a23529690d25da6c5167e23
Paras Arya <parasarya289@gmail.com>
2023-10-03 16:05:43 +0530

    feat: exercise card

--- src/Components/ExerciseCard/ExerciseCard.js
+++ src/Components/ExerciseCard/ExerciseCard.js
@@ -1,18 +1,20 @@
 import { exerciseCalories } from "../../Constants";
 import "./ExerciseCard.css";
-import { AiOutlineDelete } from "react-icons/ai";
+import { AiOutlineDelete,AiOutlineFire } from "react-icons/ai";
 
 export const ExerciseCard = ({ exercise }) => {
   return (
     <div className="exerciseCard">
+      
       <h4>{exercise.name}</h4>
       <p>{exerciseCalories[exercise.exerciseType]?.type}</p>
       <p>{exercise.duration} Min</p>
+      <p>{exercise.caloriesBurnt} <AiOutlineFire/> </p>
       <img
         src={exerciseCalories[exercise.exerciseType]?.imgSrc}
         alt={exercise.name}
       />
-      <AiOutlineDelete />
+      <AiOutlineDelete className="exerciseCard__delete"/>
     </div>
   );
 };
