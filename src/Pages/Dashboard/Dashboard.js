589b0faeac62110b0b793a634c6a57ed8b97c5d6
Paras Arya <parasarya289@gmail.com>
2023-10-03 23:27:31 +0530

    feat: change in jsx structure

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,39 +1,42 @@
 import "./Dashboard.css";
-import { AiFillFire,AiOutlineFire } from "react-icons/ai";
+import { AiOutlineFire } from "react-icons/ai";
 
 export const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div className="dashboard__card">
-        <p>
-          <AiOutlineFire />
-          174
-        </p>
-        <h4>Total Calories Burned</h4>
-      </div>
+      <h1>Dashboard</h1>
+      <div className="dashboard__container">
+        <div className="dashboard__card">
+          <p>
+            <AiOutlineFire />
+            174
+          </p>
+          <h4>Total Calories Burned</h4>
+        </div>
 
-      <div className="dashboard__card">
-        <p>
-          <AiOutlineFire />
-          174
-        </p>
-        <h4>Total Calories Consumed</h4>
-      </div>
+        <div className="dashboard__card">
+          <p>
+            <AiOutlineFire />
+            174
+          </p>
+          <h4>Total Calories Consumed</h4>
+        </div>
 
-      <div className="dashboard__card">
-        <p>
-          <AiOutlineFire />
-          174
-        </p>
-        <h4>Total Calories Goal</h4>
-      </div>
+        <div className="dashboard__card">
+          <p>
+            <AiOutlineFire />
+            174
+          </p>
+          <h4>Total Calories Goal</h4>
+        </div>
 
-      <div className="dashboard__card">
-        <p>
-          <AiOutlineFire />
-          174
-        </p>
-        <h4>Remaining Calories to Goal</h4>
+        <div className="dashboard__card">
+          <p>
+            <AiOutlineFire />
+            174
+          </p>
+          <h4>Remaining Calories to Goal</h4>
+        </div>
       </div>
     </div>
   );
