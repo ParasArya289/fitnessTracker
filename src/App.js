b9c8f5542961ac5584dd055361537a746db11371
Paras Arya <parasarya289@gmail.com>
2023-10-03 17:50:39 +0530

    fix: netlify.toml file added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { Layout } from "./Layout/Layout";
 import { Dashboard } from "./Pages/Dashboard/Dashboard";
+import { Exercises } from "./Pages/Exercises/Exercises";
+import { Foods } from "./Pages/Foods/Foods";
+import { Goals } from "./Pages/Goals/Goals";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<Dashboard/>}/>
-          <Route path="/exercises" element={<h1>Exercises</h1>}/>
-          <Route path="/foods" element={<h1>Foods</h1>}/>
-          <Route path="/goals" element={<h1>Goals</h1>}/>
-          <Route path="*" element={<h1>Page does not exist</h1>}/>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/exercises" element={<Exercises />} />
+          <Route path="/foods" element={<Foods />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="*" element={<h1>Page does not exist</h1>} />
         </Routes>
       </Layout>
     </div>
