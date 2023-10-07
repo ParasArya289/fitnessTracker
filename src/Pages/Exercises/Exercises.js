aa183afb3916a9868bcd0b6e6c7f25342cbdd509
Paras Arya <parasarya289@gmail.com>
2023-10-08 01:06:10 +0530

    feat: state management

--- src/Pages/Exercises/Exercises.js
+++ src/Pages/Exercises/Exercises.js
@@ -2,7 +2,7 @@ import { ExerciseCard } from "../../Components/ExerciseCard/ExerciseCard";
 import "./Exercises.css";
 import { RxLightningBolt } from "react-icons/rx";
 import ExerciseModal from "../../Components/Modal/ExerciseModal";
-import {useSelector } from "react-redux";
+import { useSelector, shallowEqual } from "react-redux";
 
 export const Exercises = () => {
   const exercies = [
@@ -43,13 +43,16 @@ export const Exercises = () => {
       caloriesBurnt: 540,
     },
   ];
-  const exercises = useSelector(state=>state.exercises)
-  const loading = useSelector(state=>state.loading)
+  const exercises = useSelector((state) => state.exercises, shallowEqual);
+  const loading = useSelector((state) => state.loading);
+  const exerciseLoading = useSelector((state) => state.exerciseLoading);
+
   return (
     <div className="exercises">
       <h1>Exercises</h1>
-      {loading&&<h4>Loading...</h4>}
+      {loading && <h4>Loading...</h4>}
       <div className="exericise__container">
+        {exerciseLoading && <h4>Adding...</h4>}
         {exercises.map((exercise) => (
           <ExerciseCard exercise={exercise} />
         ))}
