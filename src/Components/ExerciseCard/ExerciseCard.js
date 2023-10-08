2c73b1379bf92e64f174c1d032c5acf8982a10ce
Paras Arya <parasarya289@gmail.com>
2023-10-08 16:56:22 +0530

    feat: handling delete exercise

--- src/Components/ExerciseCard/ExerciseCard.js
+++ src/Components/ExerciseCard/ExerciseCard.js
@@ -1,20 +1,49 @@
 import { exerciseCalories } from "../../Constants";
 import "./ExerciseCard.css";
-import { AiOutlineDelete,AiOutlineFire } from "react-icons/ai";
+import { AiOutlineDelete, AiOutlineFire } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { removeEntry } from "../../dataActions";
+import { useState } from "react";
 
-export const ExerciseCard = ({ exercise }) => {
+export const ExerciseCard = ({
+  name,
+  _id,
+  exerciseType,
+  caloriesBurnt,
+  duration,
+  imageSrc,
+}) => {
+  console.log(imageSrc)
+  const [currentId, setCurrentId] = useState();
+  const dispatch = useDispatch();
+  const exerciseLoading = useSelector((state) => state.exerciseLoading);
+  const deleteExerciseHandler = () => {
+    dispatch(removeEntry({ type: "exercises", _id: _id }));
+    setCurrentId(_id);
+  };
   return (
-    <div className="exerciseCard">
-      
-      <h4>{exercise.name}</h4>
-      <p>{exerciseCalories[exercise.exerciseType]?.type}</p>
-      <p>{exercise.duration} Min</p>
-      <p>{exercise.caloriesBurnt} <AiOutlineFire/> </p>
+    <div
+      className="exerciseCard"
+      style={{
+        opacity: exerciseLoading.deleting && currentId === _id ? 0.8 : 1,
+        transition: "opacity 0.3s ease",
+      }}
+    >
+      <h4>{name}</h4>
+      <p>{exerciseCalories[exerciseType]?.type}</p>
+      <p>{duration} Min</p>
+      <p>
+        {caloriesBurnt} <AiOutlineFire />{" "}
+      </p>
       <img
-        src={exerciseCalories[exercise.exerciseType]?.imgSrc}
-        alt={exercise.name}
+        src={imageSrc || exerciseCalories[exerciseType]?.imgSrc}
+        alt={name}
       />
-      <AiOutlineDelete className="exerciseCard__delete"/>
+      <AiOutlineDelete
+        tabIndex={"0"}
+        className="exerciseCard__delete"
+        onClick={deleteExerciseHandler}
+      />
     </div>
   );
 };
