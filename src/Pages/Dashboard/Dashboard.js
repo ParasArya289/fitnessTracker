8412b229ce4205d57b7705026a6d5917c7175906
Paras Arya <parasarya289@gmail.com>
2023-10-09 14:56:58 +0530

    feat: show calculation based on data available

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,30 @@
 import "./Dashboard.css";
 import { AiOutlineFire } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export const Dashboard = () => {
+  const exercises = useSelector((state) => state.exercises);
+  const foods = useSelector((state) => state.foods);
+  const goals = useSelector((state) => state.goals);
+
+  const totalCaloriesBurned = exercises.reduce(
+    (acc, { caloriesBurnt }) => acc + caloriesBurnt,
+    0
+  );
+  const totalCaloriesConsumed = foods.reduce(
+    (acc, { calories }) => acc + calories,
+    0
+  );
+  const totalCaloriesGoal = goals.reduce(
+    (acc, { targetCaloriesValue }) => acc + targetCaloriesValue,
+    0
+  );
+  const totalCaloriesGoalRemaining = goals.reduce(
+    (acc, { targetCaloriesValue, status }) =>
+      status === "Achieved" ? acc : acc + targetCaloriesValue,
+    0
+  );
+
   return (
     <div className="dashboard">
       <h1>Dashboard</h1>
@@ -9,7 +32,7 @@ export const Dashboard = () => {
         <div className="dashboard__card">
           <p>
             <AiOutlineFire />
-            174
+            {totalCaloriesBurned}
           </p>
           <h4>Total Calories Burned</h4>
           <img src="dashboardCardBg.svg" />
@@ -18,7 +41,7 @@ export const Dashboard = () => {
         <div className="dashboard__card">
           <p>
             <AiOutlineFire />
-            174
+            {totalCaloriesConsumed}
           </p>
           <h4>Total Calories Consumed</h4>
           <img src="dashboardCardBg.svg" />
@@ -27,16 +50,16 @@ export const Dashboard = () => {
         <div className="dashboard__card">
           <p>
             <AiOutlineFire />
-            174
+            {totalCaloriesGoal}
           </p>
           <h4>Total Calories Goal</h4>
-          <img src="dashboardCardBg.svg"/>
+          <img src="dashboardCardBg.svg" />
         </div>
 
         <div className="dashboard__card">
           <p>
             <AiOutlineFire />
-            174
+            {totalCaloriesGoalRemaining}
           </p>
           <h4>Remaining Calories to Goal</h4>
           <img src="dashboardCardBg.svg" />
