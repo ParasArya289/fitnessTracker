c31f1d61ccc7c7134c4d5149da96a1f854e0946e
Paras Arya <parasarya289@gmail.com>
2023-10-15 23:58:41 +0530

    cleanup

--- src/Pages/Foods/Foods.js
+++ src/Pages/Foods/Foods.js
@@ -6,43 +6,6 @@ import { PlaceholderCard } from "../../Components/PlaceholderCard/PlaceholderCar
 import "./Foods.css";
 
 export const Foods = () => {
-  // const foods = [
-  //   {
-  //     foodName: "Banana",
-  //     calories: 105,
-  //     protein: 1.3,
-  //     carbohydrates: 27,
-  //     fat: 0.3,
-  //   },
-  //   {
-  //     foodName: "Spaghetti with Tomato Sauce",
-  //     calories: 350,
-  //     protein: 10,
-  //     carbohydrates: 70,
-  //     fat: 2,
-  //   },
-  //   {
-  //     foodName: "Grilled Salmon",
-  //     calories: 367,
-  //     protein: 40,
-  //     carbohydrates: 0,
-  //     fat: 22,
-  //   },
-  //   {
-  //     foodName: "Greek Salad",
-  //     calories: 280,
-  //     protein: 7,
-  //     carbohydrates: 10,
-  //     fat: 24,
-  //   },
-  //   {
-  //     foodName: "Cheeseburger",
-  //     calories: 350,
-  //     protein: 13,
-  //     carbohydrates: 33,
-  //     fat: 18,
-  //   },
-  // ];
   const foods = useSelector((state) => state.foods);
   const loading = useSelector((state) => state.loading);
   const foodLoading = useSelector((state) => state.foodLoading);
